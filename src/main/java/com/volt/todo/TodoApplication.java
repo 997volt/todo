0d191936f12b7d9e2a1aad3faf2f96e9b8c8a7a1
997volt <p.halaburdzin@gmail.com>
2023-02-03 22:07:49 +0100

    add simple getmapping

--- src/main/java/com/volt/todo/TodoApplication.java
+++ src/main/java/com/volt/todo/TodoApplication.java
@@ -2,12 +2,21 @@ package com.volt.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
+	@GetMapping("/greet")
+	public GreetResponse greet(){
+		return new GreetResponse("Hello");
+	}
+
+	record GreetResponse(String greet) {}
 }
