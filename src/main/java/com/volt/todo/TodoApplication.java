97cced3ad703b87fa847ec10f75a03cee45beff6
997volt <p.halaburdzin@gmail.com>
2023-02-13 22:43:54 +0100

    add more complicated json response

--- src/main/java/com/volt/todo/TodoApplication.java
+++ src/main/java/com/volt/todo/TodoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class TodoApplication {
@@ -15,8 +17,17 @@ public class TodoApplication {
 
 	@GetMapping("/greet")
 	public GreetResponse greet(){
-		return new GreetResponse("Hello");
+		return new GreetResponse(
+				"Hello",
+				List.of(5.0,3.5,2.2),
+				new Person("Alex", 10));
 	}
 
-	record GreetResponse(String greet) {}
+	record Person(String name, int age){}
+
+
+	record GreetResponse(
+			String greet,
+			List<Double> scores,
+			Person person) {}
 }
